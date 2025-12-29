5a829de130bc280a67b23423d0b4a974545e95d2
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-12-30 00:35:34 +0100

    MNE-11: Remove all AllowAnonymous

--- Mnema.Server/Controllers/ContentController.cs
+++ Mnema.Server/Controllers/ContentController.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Mnema.API;
 using Mnema.API.Content;
@@ -6,14 +5,12 @@ using Mnema.Common;
 using Mnema.Models.DTOs.Content;
 using Mnema.Models.Entities.Content;
 using Mnema.Models.Publication;
-using Mnema.Providers;
 
 namespace Mnema.Server.Controllers;
 
 public class ContentController(ISearchService searchService, IDownloadService downloadService, IServiceProvider serviceProvider): BaseApiController
 {
 
-    [AllowAnonymous]
     [HttpPost("search")]
     public Task<PagedList<SearchResult>> Search(SearchRequest searchRequest, [FromQuery] PaginationParams? pagination)
     {
@@ -22,7 +19,6 @@ public class ContentController(ISearchService searchService, IDownloadService do
         return searchService.Search(searchRequest, pagination, HttpContext.RequestAborted);
     }
 
-    [AllowAnonymous]
     [HttpGet("series-info")]
     public async Task<ActionResult<Series>> GetSeriesInfo([FromQuery] Provider provider, [FromQuery] string id)
     {
@@ -41,8 +37,7 @@ public class ContentController(ISearchService searchService, IDownloadService do
         
         return Ok(await repository.SeriesInfo(request, HttpContext.RequestAborted));
     }
-    
-    [AllowAnonymous]
+
     [HttpGet("chapter-urls")]
     public async Task<ActionResult<List<DownloadUrl>>> GetChapterUrls([FromQuery] Provider provider, [FromQuery] string id)
     {
@@ -64,29 +59,26 @@ public class ContentController(ISearchService searchService, IDownloadService do
         return Ok(await repository.ChapterUrls(chapter, HttpContext.RequestAborted));
     }
 
-    [AllowAnonymous]
     [HttpPost("download")]
     public async Task<IActionResult> Download(DownloadRequestDto request)
     {
-        request.UserId = Guid.Parse("2f461b21-85f0-4e1e-b64b-fb48c774cdb6");
+        request.UserId = UserId;
         
         await downloadService.StartDownload(request);
 
         return Ok();
     }
-    
-    [AllowAnonymous]
+
     [HttpGet("stats")]
     public async Task<ActionResult<IEnumerable<DownloadInfo>>> Stats()
     {
-        return Ok(await downloadService.GetCurrentContent(Guid.Parse("2f461b21-85f0-4e1e-b64b-fb48c774cdb6")));
+        return Ok(await downloadService.GetCurrentContent(UserId));
     }
-    
-    [AllowAnonymous]
+
     [HttpPost("stop")]
     public async Task<IActionResult> Stop(StopRequestDto request)
     {
-        request.UserId = Guid.Parse("2f461b21-85f0-4e1e-b64b-fb48c774cdb6");
+        request.UserId = UserId;
 
         await downloadService.CancelDownload(request);
         
