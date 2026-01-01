a9d66861faf9c0b5a357015ca14bfb9c13e77a1e
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-01-01 20:37:12 +0100

    Fix enum values for AgeRating

--- Mnema.Models/Publication/AgeRating.cs
+++ Mnema.Models/Publication/AgeRating.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Xml.Serialization;
 
 namespace Mnema.Models.Publication;
 
@@ -9,35 +10,65 @@ namespace Mnema.Models.Publication;
 /// <remarks>Copied from https://github.com/Kareadita/Kavita/blob/develop/API/Entities/Enums/AgeRating.cs</remarks>
 public enum AgeRating
 {
+    [XmlEnum("Unknown")]
     [Description("Unknown")]
     Unknown = 0,
+
+    [XmlEnum("Rating Pending")]
     [Description("Rating Pending")]
     RatingPending = 1,
+
+    [XmlEnum("Early Childhood")]
     [Description("Early Childhood")]
     EarlyChildhood = 2,
+
+    [XmlEnum("Everyone")]
     [Description("Everyone")]
     Everyone = 3,
+
+    [XmlEnum("G")]
     [Description("G")]
     G = 4,
+
+    [XmlEnum("Everyone 10+")]
     [Description("Everyone 10+")]
     Everyone10Plus = 5,
+
+    [XmlEnum("PG")]
     [Description("PG")]
     // ReSharper disable once InconsistentNaming
     PG = 6,
+
+    [XmlEnum("Kids to Adults")]
     [Description("Kids to Adults")]
     KidsToAdults = 7,
+
+    [XmlEnum("Teen")]
     [Description("Teen")]
     Teen = 8,
+
+    [XmlEnum("MA15+")]
     [Description("MA15+")]
     Mature15Plus = 9,
+
+    [XmlEnum("Mature 17+")]
     [Description("Mature 17+")]
     Mature17Plus = 10,
+
+    [XmlEnum("M")]
     [Description("M")]
     Mature = 11,
+
+    [XmlEnum("R18+")]
     [Description("R18+")]
     R18Plus = 12,
+
+    [XmlEnum("Adults Only 18+")]
     [Description("Adults Only 18+")]
     AdultsOnly = 13,
+
+    [XmlEnum("X18+")]
     [Description("X18+")]
     X18Plus = 14,
 }
+
