bdc3bd9f174518133adccabcd91a25208b6e68fd
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-01-01 19:43:59 +0100

    Register a NoOpContentManager for Nyaa

---
+++ Mnema.Providers/NoOpContentManager.cs
@@ -0,0 +1,37 @@
+using Mnema.API.Content;
+using Mnema.Models.DTOs.Content;
+
+namespace Mnema.Providers;
+
+public class NoOpContentManager: IContentManager
+{
+    public Task Download(DownloadRequestDto request)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StopDownload(StopRequestDto request)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task MoveToDownloadQueue(string id)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task<IEnumerable<IContent>> GetAllContent()
+    {
+        return Task.FromResult<IEnumerable<IContent>>([]);
+    }
+
+    public Task<MessageDto> RelayMessage(MessageDto message)
+    {
+        return Task.FromResult(new MessageDto
+        {
+            Provider = message.Provider,
+            ContentId = message.ContentId,
+            Type = message.Type,
+        });
+    }
+}
