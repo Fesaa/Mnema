090839d201d78fe5eda6583a1ba39fd5916576f1
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-01-05 22:26:32 +0100

    Try adding this manifest

--- Mnema.Frontend/Web/src/app/button-grid/button-grid.component.ts
+++ Mnema.Frontend/Web/src/app/button-grid/button-grid.component.ts
@@ -1,21 +1,16 @@
 import {Component, computed, inject, input, TemplateRef, viewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Breakpoint, UtilityService} from "../_services/utility.service";
-import {NavigationBehaviorOptions, NavigationExtras, Router, UrlTree} from "@angular/router";
 import {Button, ButtonGroup, ButtonGroupService} from "./button-group.service";
-import {ModalService} from "../_services/modal.service";
 import {
-  ListSelectionItem,
-  ListSelectModalComponent
+  ListSelectionItem
 } from "../shared/_component/list-select-modal/list-select-modal.component";
-import {tap} from "rxjs";
-import {UpdateBadgeDirective} from "../_directives/update-badge.directive";
 import {BadgeComponent} from "../shared/_component/badge/badge.component";
 
 @Component({
   selector: 'app-button-grid',
   standalone: true,
-  imports: [CommonModule, UpdateBadgeDirective, BadgeComponent],
+  imports: [CommonModule, BadgeComponent],
   templateUrl: './button-grid.component.html',
   styleUrls: ['./button-grid.component.scss']
 })
