088bc7f1b68f4d988a2f82025160b01056c837da
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-07-27 00:02:39 +0200

    Sqlite Migration

---
+++ Mnema.Database/SqliteMigrations/20260726215445_SqliteAddPageDefaultOptions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mnema.Database.SqliteMigrations
+{
+    /// <inheritdoc />
+    public partial class SqliteAddPageDefaultOptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DefaultOptions",
+                table: "Pages",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "{}");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultOptions",
+                table: "Pages");
+        }
+    }
+}
