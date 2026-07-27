961036ae8b03ae2afd09c4c6a3c798c24476081b
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-07-27 15:04:54 +0200

    Sqlite Migration

---
+++ Mnema.Database/SqliteMigrations/20260727130038_SqliteAddExternalDownloads.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mnema.Database.SqliteMigrations
+{
+    /// <inheritdoc />
+    public partial class SqliteAddExternalDownloads : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExternalDownloads",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ExternalId = table.Column<string>(type: "TEXT", nullable: false),
+                    BaseDir = table.Column<string>(type: "TEXT", nullable: false),
+                    Provider = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Metadata = table.Column<string>(type: "TEXT", nullable: false, defaultValue: "{}"),
+                    CreatedUtc = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModifiedUtc = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Files = table.Column<string>(type: "TEXT", nullable: false, defaultValue: "[]")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExternalDownloads", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExternalDownloads");
+        }
+    }
+}
