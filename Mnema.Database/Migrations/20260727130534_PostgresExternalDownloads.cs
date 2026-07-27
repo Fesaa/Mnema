6a4d29b34003e55c0e6a4088b3a738843789646d
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-07-27 15:05:54 +0200

    Postgres Migration

---
+++ Mnema.Database/Migrations/20260727130534_PostgresExternalDownloads.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mnema.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostgresExternalDownloads : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Roles",
+                table: "AuthKeys",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+
+            migrationBuilder.CreateTable(
+                name: "ExternalDownloads",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ExternalId = table.Column<string>(type: "text", nullable: false),
+                    BaseDir = table.Column<string>(type: "text", nullable: false),
+                    Provider = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Metadata = table.Column<string>(type: "TEXT", nullable: false, defaultValue: "{}"),
+                    CreatedUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Files = table.Column<string>(type: "TEXT", nullable: false, defaultValue: "[]")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExternalDownloads", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExternalDownloads");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Roles",
+                table: "AuthKeys",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+        }
+    }
+}
