8592ea66962d0ff1b52d77c374c5e068629ce80d
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-08-02 16:22:35 +0200

    Postgres migration

---
+++ Mnema.Database/Migrations/20260802142143_PostgresUpdateMetadataMappings.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mnema.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostgresUpdateMetadataMappings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MetadataFieldMappings",
+                table: "Preferences",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "{}");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Roles",
+                table: "AuthKeys",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MetadataFieldMappings",
+                table: "Preferences");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Roles",
+                table: "AuthKeys",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+        }
+    }
+}
