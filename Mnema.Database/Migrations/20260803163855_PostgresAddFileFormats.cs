0c06d772fbbad1e1422e315650572482cbdd7dbb
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-08-03 18:39:04 +0200

    Add Postgres migration

---
+++ Mnema.Database/Migrations/20260803163855_PostgresAddFileFormats.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mnema.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostgresAddFileFormats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ChapterFileFormat",
+                table: "Preferences",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OneShotFileFormat",
+                table: "Preferences",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Roles",
+                table: "AuthKeys",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChapterFileFormat",
+                table: "Preferences");
+
+            migrationBuilder.DropColumn(
+                name: "OneShotFileFormat",
+                table: "Preferences");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Roles",
+                table: "AuthKeys",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+        }
+    }
+}
