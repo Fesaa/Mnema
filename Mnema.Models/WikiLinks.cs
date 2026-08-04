82551598dcf36115deafda34a290a0d0dd10a282
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-08-04 16:37:28 +0200

    Fix wiki base dump autocomplete

--- Mnema.Models/WikiLinks.cs
+++ Mnema.Models/WikiLinks.cs
@@ -8,7 +8,7 @@ public static class WikiLinks
     private static readonly bool IsDev =
         Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")?.Equals("Development") == true;
 
-    public static readonly string WikiBase = IsDev ? "http://localhost:63343/Docs/preview/" : "https://mnema.gitbook.io/mnema/";
+    public static readonly string WikiBase = IsDev ? "http://localhost:63343/Docs/preview/" : "https://fesaa.github.io/Mnema/";
     public static readonly string NamingFormatDocumentation = WikiBase + "naming.html";
     public static readonly string ServerSettings = WikiBase + "server.html";
     public static readonly string MetadataProvidersMangaBaka = ServerSettings + "#mangabaka";
