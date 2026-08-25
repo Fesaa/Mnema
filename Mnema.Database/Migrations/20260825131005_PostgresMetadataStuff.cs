6cfe106e35604ffe31afac6c7dd86e234dcbfcdc
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-08-25 15:11:35 +0200

    Oops....

---
+++ Mnema.Database/Migrations/20260825131005_PostgresMetadataStuff.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mnema.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostgresMetadataStuff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UnMatchedChapters",
+                table: "MonitoredSeries",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ComicInfo",
+                table: "MonitoredChapters",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Files",
+                table: "DirectoryImportResults",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Roles",
+                table: "AuthKeys",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnMatchedChapters",
+                table: "MonitoredSeries");
+
+            migrationBuilder.DropColumn(
+                name: "ComicInfo",
+                table: "MonitoredChapters");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Files",
+                table: "DirectoryImportResults",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Roles",
+                table: "AuthKeys",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+        }
+    }
+}
