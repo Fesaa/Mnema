14e99f511da892c5cdc49f97984dab7a5c3efa42
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-09-18 20:38:23 +0200

    Add postgres migration

---
+++ Mnema.Database/Migrations/20260918183810_AddDroppedContent.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mnema.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDroppedContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Files",
+                table: "DirectoryImportResults",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Roles",
+                table: "AuthKeys",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+
+            migrationBuilder.CreateTable(
+                name: "DroppedContent",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MonitoredSeriesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Files = table.Column<string>(type: "TEXT", nullable: false, defaultValue: "[]")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DroppedContent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DroppedContent_MonitoredSeries_MonitoredSeriesId",
+                        column: x => x.MonitoredSeriesId,
+                        principalTable: "MonitoredSeries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DroppedContent_MonitoredSeriesId",
+                table: "DroppedContent",
+                column: "MonitoredSeriesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DroppedContent");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Files",
+                table: "DirectoryImportResults",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Roles",
+                table: "AuthKeys",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>(),
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldDefaultValue: new List<string>());
+        }
+    }
+}
